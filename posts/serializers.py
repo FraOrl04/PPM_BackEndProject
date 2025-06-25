7f826329a9936070519a5de3bd6037c0d04f9401
FrancescoOrlando04 <francesco.orlando3@stud.unifi.it>
2025-06-25 18:12:23 +0200

    modifica dei file serializers.py views.py per la creazione delle viste e creazione del file premissions.py per la gestione dei super users

---
+++ posts/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Post, Comment, Like
+from accounts.serializers import UserSerializer
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'text', 'created_at']
+        read_only_fields = ['author', 'created_at']
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = '__all__'
+        read_only_fields = ['user']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    author = UserSerializer(read_only=True)
+    likes = LikeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'content', 'created_at', 'comments', 'likes']
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+from .models import Post, Comment, Like
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer
+from .permissions import IsAuthorOrReadOnly
+from .permissions import IsAuthorOrAdmin
+from .permissions import IsAdminUser
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly, IsAuthorOrAdmin]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrAdmin]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+from rest_framework import status
+from rest_framework.response import Response
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        post_id = request.data.get('post')
+        user = request.user
+
+        # evita duplicazione del like
+        if Like.objects.filter(post_id=post_id, user=user).exists():
+            return Response({'detail': 'You already liked this post.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        like = Like.objects.create(post_id=post_id, user=user)
+        serializer = self.get_serializer(like)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class AdminPostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+class AdminCommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
