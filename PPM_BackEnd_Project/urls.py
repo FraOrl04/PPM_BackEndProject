c6df8c2066f84dafeb9cd445763e1a3c121020f7
FrancescoOrlando04 <francesco.orlando3@stud.unifi.it>
2025-06-25 18:17:07 +0200

    modifica del file settings.py e urls.py del origetto principale per la gestione del backend e frontend

--- PPM_BackEnd_Project/urls.py
+++ PPM_BackEnd_Project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for PPM_BackEnd_Project project.
+URL configuration for SocialMediaAPI project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('posts.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
