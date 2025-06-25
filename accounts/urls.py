48bf52d596a54664cf497912fc3a03a3eae86a64
FrancescoOrlando04 <francesco.orlando3@stud.unifi.it>
2025-06-25 18:08:45 +0200

    modifica al file token_views.py e urls.py per la gestione dei super user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import UserList, RegisterUserView, FollowUserView, UserAdminViewSet, AdminSelfView
 from rest_framework.routers import DefaultRouter
-
+from .token_views import CustomTokenObtainPairView
 
 urlpatterns = [
     path('', UserList.as_view()),
     path('register/', RegisterUserView.as_view()),
     path('follow/<str:username>/', FollowUserView.as_view(), name='follow-user'),
+path('token/', CustomTokenObtainPairView.as_view(), name='custom_token_obtain_pair'),
     path('admin-users/', AdminSelfView.as_view()),
 
 ]
