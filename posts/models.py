059a524e061c863e328a58da1549badc7ceac5ca
FrancescoOrlando04 <francesco.orlando3@stud.unifi.it>
2025-06-25 18:10:36 +0200

    creazione dei modelli per post like e commenti nell file models.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, related_name="likes", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('post', 'user')  # previene like duplicati
+
+
+from django.db import models
 
 # Create your models here.
