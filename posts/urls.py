f2696980a5f5a3bc4f6bf45b4bffc99cd20c68da
FrancescoOrlando04 <francesco.orlando3@stud.unifi.it>
2025-07-06 20:47:34 +0200

    commit finale per immagini e like rimossi

--- posts/urls.py
+++ posts/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import (
     PostViewSet, CommentViewSet, LikeViewSet,
     AdminPostViewSet, AdminCommentViewSet
 )
-
 like_delete = LikeViewSet.as_view({'delete': 'destroy'})
-
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet)
@@ -15,6 +12,4 @@ router.register(r'admin-posts', AdminPostViewSet, basename='admin-posts')
 router.register(r'admin-comments', AdminCommentViewSet, basename='admin-comments')
 
 
-urlpatterns = router.urls + [
-    path('likes/delete/<int:pk>/', like_delete, name='like-delete'),
-]
+urlpatterns = router.urls
