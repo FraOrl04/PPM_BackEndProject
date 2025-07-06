1c4c34b030b5970c6a13047b09c9321957d911f3
FrancescoOrlando04 <francesco.orlando3@stud.unifi.it>
2025-07-06 16:56:09 +0200

    modifica porta di accesso backend frontend

--- PPM_BackEnd_Project/settings.py
+++ PPM_BackEnd_Project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'PPM_BackEnd_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'kJfPuRXxUrXVofhWlTrcTJDGSXQMsYMO',
+        'HOST': 'trolley.proxy.rlwy.net',
+        'PORT': '54881',
     }
 }
 
